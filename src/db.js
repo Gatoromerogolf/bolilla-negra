777a3a660a81322526294d2597f38bd422aa1f71
Gatoromerogolf <ruben.e.garcia@gmail.com>
2024-08-18 22:46:33 -0300

    acceso a a base de datos

--- src/db.js
+++ src/db.js
@@ -3,14 +3,17 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 // Crear conexión usando la URL completa
-const conexion = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB,
-  connectTimeout: 10000
-});
+// const conexion = mysql.createConnection({
+//   host: process.env.MYSQL_HOST,
+//   port: process.env.MYSQL_PORT,
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DB,
+//   connectTimeout: 10000
+// });
+
+// Crear conexión usando la URL completa
+const conexion = mysql.createConnection(process.env.MYSQL_URL);
 
 conexion.connect((err) => {
   if (err) {
